c48d7b7cb88c3d0ea351f0a99a0d99fe8fe0b45a
chaniYehudit <p0548453160@gmail.com>
2022-08-15 13:41:30 +0300

    aaaa

--- nestProject/src/app.module.ts
+++ nestProject/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminModule} from './admin/admin.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { SystemModule } from './system/system.module';
 import { UserModule } from './user/user.module';
 
 
+
 @Module({
-  imports: [ AdminModule, MongooseModule.forRoot('mongodb://localhost:27017/nest'), SystemModule, UserModule]
+  imports: [ AdminModule, MongooseModule.forRoot('mongodb://localhost:27017/nest'), UserModule]
 })
 export class AppModule {}
