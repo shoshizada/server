b1496bc33382fea7ac3329e35b267133465adb15
chaniYehudit <p0548453160@gmail.com>
2022-08-30 13:59:17 +0300

    add request service

--- nestProject/src/request/request.module.ts
+++ nestProject/src/request/request.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RequestService } from './request.service';
 import { RequestController } from './request.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RequestSchema } from './request.model';
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: 'Request', schema: ReadableStream}])],
+  imports: [MongooseModule.forFeature([{name: 'Request', schema: RequestSchema}])],
   providers: [RequestService],
   controllers: [RequestController]
 })
